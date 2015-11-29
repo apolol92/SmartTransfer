c6b5ea5aae404fdab38b054f5f381087e612663b
apolol92 <apolol92@web.de>
2015-11-29 21:46:01 +0100

    Added DServer..

---
+++ Code/SmartTransferServer/SmartTransferServer/DServer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartTransferServer
+{
+    /// <summary>
+    /// This is the most important class in this project.
+    /// With this class we will control and do the transfer between smartphone and PC.
+    /// </summary>
+    class DServer
+    {
+        /// <summary>
+        /// This is the tcp-server
+        /// </summary>
+        TcpListener server = null;
+        /// <summary>
+        /// This ist the SERVER_PORT
+        /// </summary>
+        private const int SERVER_PORT = 1314;
+        /// <summary>
+        /// Initialize our server
+        /// </summary>
+        public DServer()
+        {
+           try
+            {
+                this.server = new TcpListener(SERVER_PORT);
+                this.server.Start();
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Extract the requestCommandStr
+        /// </summary>
+        /// <param name="client">A Reference of the client</param>
+        /// <returns></returns>
+        public String getRequestCommandStr(TcpClient client)
+        {
+            Byte[] bytes = new Byte[256];
+            NetworkStream stream = client.GetStream();
+            int i;
+            String RequestCommandStr = "";
+            // Loop to receive all the data sent by the client.
+            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                // Translate data bytes to a ASCII string.
+                RequestCommandStr += System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+            }
+            return RequestCommandStr;
+        }
+
+        /// <summary>
+        /// Use this method to run the server
+        /// </summary>
+        public void run()
+        {
+            
+            while (true)
+            {
+                //Wait for a client
+                TcpClient client = server.AcceptTcpClient();
+                //Get RequestCommand as String
+                String RequestCommandStr = getRequestCommandStr(client);
+                //Extract RequestCommandStr
+
+            }
+        }
+    }
+}
