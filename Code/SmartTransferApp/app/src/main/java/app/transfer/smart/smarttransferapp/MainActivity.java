38b4658525df10f636189e0b75802a8643a99227
apolol92 <apolol92@web.de>
2015-12-06 22:16:41 +0100

    Made app runnable..

---
+++ Code/SmartTransferApp/app/src/main/java/app/transfer/smart/smarttransferapp/MainActivity.java
@@ -0,0 +1,15 @@
+package app.transfer.smart.smarttransferapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by apolol92 on 06.12.2015.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- Code/SmartTransferApp/app/src/main/java/app/transfer/smart/smarttransferapp/WelcomeActivity.java
+++ Code/SmartTransferApp/app/src/main/java/app/transfer/smart/smarttransferapp/WelcomeActivity.java
@@ -11,7 +11,6 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        //test
     }
 
     @Override
